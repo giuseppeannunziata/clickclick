9ca80fe7b58bb3e8159137874ee85d3de980cfd8
sabob1 <sabob1@ec524382-5d46-0410-b920-5d724188c503>
2008-07-03 15:33:29 +0000

    initial checkin

---
+++ clickclick/jquery/src/net/sf/clickclick/jquery/dialog/JQDialog.java
@@ -0,0 +1,55 @@
+package net.sf.clickclick.jquery.dialog;
+
+import net.sf.click.MockContext;
+import net.sf.click.control.CssImport;
+import net.sf.click.control.JavascriptImport;
+import net.sf.click.util.PageImports;
+import net.sf.clickclick.control.html.Div;
+
+/**
+ *
+ * @author Bob Schellink
+ */
+public class JQDialog extends Div {
+
+    private String options;
+    
+    public JQDialog() {
+        this(null);
+    }
+
+    public JQDialog(String name) {
+        super(name);
+        setAttribute("class", "flora");
+    }
+
+    public void onHtmlImports(PageImports pageImports) {
+        String contextPath = getContext().getRequest().getContextPath();
+        CssImport cssImport = new CssImport(contextPath + "/clickclick/jquery/flora.dialog.css");
+        cssImport.setAttribute("media", "screen");
+        pageImports.add(cssImport);
+        cssImport = new CssImport(contextPath + "/clickclick/jquery/flora.resizable.css");
+        cssImport.setAttribute("media", "screen");
+        pageImports.add(cssImport);
+
+        JavascriptImport jsImport = new JavascriptImport(contextPath
+            + "/clickclick/jquery/jquery-1.2.6.js");
+        pageImports.add(jsImport);
+
+        jsImport = new JavascriptImport(contextPath + "/clickclick/jquery/jquery.ui.all.js");
+        pageImports.add(jsImport);
+    }
+
+    public String getOptions() {
+        return options;
+    }
+
+    public void setOptions(String options) {
+        this.options = options;
+    }
+
+    public static void main(String[] args) {        
+        MockContext.initContext();
+        JQDialog dialog = new JQDialog();
+    }
+}
