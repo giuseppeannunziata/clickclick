d8b3cf3d9e15850466fb8e886f7c0b740bcf6454
sabob1 <sabob1@ec524382-5d46-0410-b920-5d724188c503>
2008-07-03 15:28:47 +0000

    initial checkin

---
+++ trunk/clickclick/jquery-example/src/net/sf/clickclick/examples/jquery/page/ajax/FormPage.java
@@ -0,0 +1,9 @@
+package net.sf.clickclick.examples.jquery.page.ajax;
+
+import net.sf.clickclick.examples.jquery.page.BorderPage;
+
+public class FormPage extends BorderPage {
+
+    public String title = "Admin 1 Page";
+
+}
