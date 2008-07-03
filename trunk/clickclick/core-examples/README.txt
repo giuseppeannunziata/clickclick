b1f7ec2a6411c99e9e4008524f1d8802fa3ecbaf
sabob1 <sabob1@ec524382-5d46-0410-b920-5d724188c503>
2008-07-03 15:24:19 +0000

    initial checkin

---
+++ trunk/clickclick/core-examples/README.txt
@@ -0,0 +1,5 @@
+Dependencies:
+
+  - click-core-1.5-M2.jar
+  - click-extras-1.5-M2.jar
+  - servlet-api-2.3.jar
---
+++ trunk/clickclick/core-examples/src/net/sf/clickclick/examples/nav/MenuBuilder.java
@@ -0,0 +1,93 @@
+package net.sf.clickclick.examples.nav;
+
+import net.sf.click.Context;
+import net.sf.click.extras.control.Menu;
+import net.sf.clickclick.control.menu.FlexiMenu;
+
+public class MenuBuilder {
+
+    private static class MenuHolder {
+        private static Menu INSTANCE = createMenus();
+    }
+
+    public static Menu getMenus() {
+        return MenuHolder.INSTANCE;
+    }
+
+    private static Menu createMenus() {
+        FlexiMenu rootMenu = new FlexiMenu("rootMenu");
+
+        FlexiMenu menu = createMenu("Home", "home.htm");
+        rootMenu.add(menu);
+
+        menu = createMenu("Controls");
+        rootMenu.add(menu);
+
+        FlexiMenu subMenu = createMenu("Basics", "controls/html-basics.htm");
+        menu.add(subMenu);
+
+        subMenu = createMenu("Menu", "controls/menu.htm");
+        menu.add(subMenu);
+
+        menu = createMenu("Layout");
+        rootMenu.getChildren().add(menu);
+
+        subMenu = createMenu("Vertical Panel", "layout/vertical-panel-demo.htm");
+        menu.getChildren().add(subMenu);
+
+        subMenu = createMenu("Horizontal Panel", "layout/horizontal-panel-demo.htm");
+        menu.getChildren().add(subMenu);
+
+        subMenu = createMenu("Grid", "layout/grid-layout-demo.htm");
+        menu.getChildren().add(subMenu);
+
+        Context context = Context.getThreadLocalContext();
+        Class pageClass = context.getPageClass(context.getResourcePath());
+
+        if (pageClass != null) {
+            String pageClassName = pageClass.getName();
+            String srcPath = pageClassName.replace('.', '/') + ".java";
+
+            menu = createMenu("Page Java", "source-viewer.htm?filename=WEB-INF/classes/" + srcPath);
+            menu.setAttribute("target", "_blank");
+            rootMenu.add(menu);
+
+            menu = createMenu("Page HTML", "source-viewer.htm?filename=" + context.getPagePath(pageClass));
+            menu.setAttribute("target", "_blank");
+            rootMenu.add(menu);
+        }
+
+        return rootMenu;
+    }
+
+    private static FlexiMenu createMenu(String label) {
+        FlexiMenu menu = new FlexiMenu();
+        menu.setLabel(label);
+        menu.setTitle(label);
+        return menu;
+    }
+
+    private static FlexiMenu createMenu(String label, String path) {
+        FlexiMenu menu = new FlexiMenu();
+        menu.setLabel(label);
+        menu.setPath(path);
+        menu.setTitle(label);
+        return menu;
+    }
+
+    private static FlexiMenu createNamelessMenu(String label, String path, String title) {
+        FlexiMenu menu = new FlexiMenu();
+        menu.setLabel(label);
+        menu.setTitle(title);
+        menu.setPath(path);
+        return menu;
+    }
+
+    private static FlexiMenu createMenu(String name, String label, String path, String title) {
+        FlexiMenu menu = new FlexiMenu(name);
+        menu.setLabel(label);
+        menu.setTitle(title);
+        menu.setPath(path);
+        return menu;
+    }
+}
