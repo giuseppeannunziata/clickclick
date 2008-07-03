72508df8c08516564168ea3537ca7c2e9996c117
sabob1 <sabob1@ec524382-5d46-0410-b920-5d724188c503>
2008-07-03 16:04:15 +0000

    jquery-examples initial checkin

---
+++ clickclick/jquery-examples/src/net/sf/clickclick/examples/jquery/page/controls/MenuPage.java
@@ -0,0 +1,9 @@
+package net.sf.clickclick.examples.jquery.page.controls;
+
+import net.sf.clickclick.examples.jquery.page.BorderPage;
+
+public class MenuPage extends BorderPage {
+
+    public String title = "User 2 Page";
+
+}
