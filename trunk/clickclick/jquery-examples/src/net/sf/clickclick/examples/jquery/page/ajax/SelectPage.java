b7edf56645c9a12c6332839f216f87083bb71c9f
sabob1 <sabob1@ec524382-5d46-0410-b920-5d724188c503>
2008-07-03 16:04:15 +0000

    jquery-examples initial checkin

---
+++ trunk/clickclick/jquery-examples/src/net/sf/clickclick/examples/jquery/page/ajax/SelectPage.java
@@ -0,0 +1,9 @@
+package net.sf.clickclick.examples.jquery.page.ajax;
+
+import net.sf.clickclick.examples.jquery.page.BorderPage;
+
+public class SelectPage extends BorderPage {
+
+    public String title = "Admin 2 Page";
+
+}
---
+++ trunk/clickclick/jquery-examples/src/net/sf/clickclick/examples/jquery/page/controls/AccordionPage.java
@@ -0,0 +1,9 @@
+package net.sf.clickclick.examples.jquery.page.controls;
+
+import net.sf.clickclick.examples.jquery.page.BorderPage;
+
+public class AccordionPage extends BorderPage {
+
+    public String title = "Accordion";
+
+}
