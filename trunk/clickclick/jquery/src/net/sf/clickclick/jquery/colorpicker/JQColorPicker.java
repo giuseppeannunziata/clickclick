3aaa3defcf6fce78e08df72b75b3449912309f61
sabob1 <sabob1@ec524382-5d46-0410-b920-5d724188c503>
2008-07-08 23:25:31 +0000

    new controls

---
+++ trunk/clickclick/jquery/src/net/sf/clickclick/jquery/colorpicker/JQColorPicker.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.sf.clickclick.jquery.colorpicker;
+
+/**
+ *
+ * @author Bob Schellink
+ */
+public class JQColorPicker {
+
+}
