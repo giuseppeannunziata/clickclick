8fbbacb3b3f29659e9ed4a3ebb0a5b3b7f258138
sabob1 <sabob1@ec524382-5d46-0410-b920-5d724188c503>
2008-07-09 21:52:35 +0000

    colorpicker example added

---
+++ trunk/clickclick/jquery-examples/src/net/sf/clickclick/examples/jquery/page/controls/ColorPickerPage.java
@@ -0,0 +1,20 @@
+package net.sf.clickclick.examples.jquery.page.controls;
+
+import net.sf.click.control.Form;
+import net.sf.click.control.Submit;
+import net.sf.clickclick.examples.jquery.page.BorderPage;
+import net.sf.clickclick.jquery.colorpicker.JQColorPicker;
+
+public class ColorPickerPage extends BorderPage {
+
+    private Form form = new Form("form");
+
+    public void onInit() {
+        JQColorPicker colorPicker = new JQColorPicker("colorPicker");
+        colorPicker.setValue("a61b1b");
+        form.add(colorPicker);
+        
+        form.add(new Submit("submit"));
+        addControl(form);
+    }
+}
