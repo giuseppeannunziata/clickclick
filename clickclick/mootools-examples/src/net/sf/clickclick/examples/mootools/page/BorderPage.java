6cda873c1d10b2fa4ed496847a19031bd39d4b4b
sabob1 <sabob1@ec524382-5d46-0410-b920-5d724188c503>
2008-07-10 16:42:45 +0000

    mootools-examples checkin

---
+++ clickclick/mootools-examples/src/net/sf/clickclick/examples/mootools/page/BorderPage.java
@@ -0,0 +1,16 @@
+package net.sf.clickclick.examples.mootools.page;
+
+import net.sf.click.extras.control.Menu;
+
+public class BorderPage extends BasePage {
+
+    public Menu rootMenu = Menu.getRootMenu();
+
+    /**
+     * @see #getTemplate()
+     */
+    public String getTemplate() {
+        return "border-template.htm";
+    }
+
+}
