acbdcae380bed411e932783ecfd51954ff05e53d
ChHighway <ChHighway@ec524382-5d46-0410-b920-5d724188c503>
2008-10-21 21:24:27 +0000

    removed StyledBooleanSelect added samplepage for styledOption updated booleanselect page

--- trunk/clickclick/core-examples/src/net/sf/clickclick/examples/page/controls/BooleanSelectPage.java
+++ trunk/clickclick/core-examples/src/net/sf/clickclick/examples/page/controls/BooleanSelectPage.java
@@ -9,33 +9,27 @@ import net.sf.click.control.Select;
 import net.sf.click.control.Submit;
 import net.sf.click.extras.control.PageSubmit;
 import net.sf.clickclick.control.BooleanSelect;
-import net.sf.clickclick.control.panel.HorizontalPanel;
 import net.sf.clickclick.examples.page.BorderPage;
 
 public class BooleanSelectPage extends BorderPage {
 
-    public Form                 locForm        = new Form();
-    public Form                 form           = new Form();
-    private FieldSet            unstyled       = new FieldSet("BooleanSelect");
-    private FieldSet            styled         = new FieldSet("StyledBooleanSelect");
-    private Select              locale         = new Select("Select Locale");
-    private HorizontalPanel     hp             = new HorizontalPanel("panel");
-    private Submit              sbOk           = new Submit("ok", "  OK  ", this, "onSubmitClick");
+    public Form           locForm    = new Form();
+    public Form           form       = new Form();
+    private FieldSet      unstyled   = new FieldSet("BooleanSelect");
+    private Select        locale     = new Select("Select Locale");
+    private Submit        sbOk       = new Submit("ok", "  OK  ", this, "onSubmitClick");
 
-    private BooleanSelect       twostate       = new BooleanSelect("Twostate");
-    private BooleanSelect       tristate       = new BooleanSelect("Tristate", true);
-    private BooleanSelect       custom         = new BooleanSelect("Custom labels");
-    private BooleanSelect       notationTF     = new BooleanSelect("True/False");
-    private BooleanSelect       notationYN     = new BooleanSelect("Yes/No");
-    private BooleanSelect       notationOO     = new BooleanSelect("On/Off");
-    private BooleanSelect       notationAI     = new BooleanSelect("Active/Inactive");
-    private BooleanSelect       notationOC     = new BooleanSelect("Open/Closed");
+    private BooleanSelect twostate   = new BooleanSelect("Twostate");
+    private BooleanSelect tristate   = new BooleanSelect("Tristate", true);
+    private BooleanSelect custom     = new BooleanSelect("Custom labels");
 
-    private StyledBooleanSelect styledTwostate = new StyledBooleanSelect("s2", "Styled Twostate");
-    private StyledBooleanSelect styledTristate = new StyledBooleanSelect("s3", "Styled Tristate", BooleanSelect.ACTIVEINACTIVE, true);
-    private StyledBooleanSelect styledCustom   = new StyledBooleanSelect("sCust", "Custom Style");
+    private BooleanSelect notationTF = new BooleanSelect("True/False");
+    private BooleanSelect notationYN = new BooleanSelect("Yes/No");
+    private BooleanSelect notationOO = new BooleanSelect("On/Off");
+    private BooleanSelect notationAI = new BooleanSelect("Active/Inactive");
+    private BooleanSelect notationOC = new BooleanSelect("Open/Closed");
 
-    public String               result         = "";
+    public String         result     = "";
 
     public void onInit() {
         // -- personalizing BooleanSelect fields
@@ -47,23 +41,11 @@ public class BooleanSelectPage extends BorderPage {
         notationAI.setNotation(BooleanSelect.ACTIVEINACTIVE);
         notationOC.setNotation(BooleanSelect.OPENCLOSED);
 
-        styledTwostate.setOptionFalseStyleClass("falseOption");
-
-        styledCustom.setNotation(BooleanSelect.OPENCLOSED);
-        styledCustom.setTristate(true);
-        styledCustom.addStyleClass("custom");
-        styledCustom.setOptionUnsetStyleClass("custom");
-        styledCustom.setOptionTrueStyleClass("custom");
-        styledCustom.setOptionFalseStyleClass("custom");
-        styledCustom.setOptionUnsetStyleAttributes("height: 26px; padding-left: 25px;");
-        styledCustom.setOptionTrueStyleAttributes("height: 26px; padding-left: 25px; background-image: url(/clickclick-core-examples/assets/images/lockopen.png);");
-        styledCustom.setOptionFalseStyleAttributes("height: 26px; padding-left: 25px; background-image: url(/clickclick-core-examples/assets/images/lockclosed.png);");
-        styledCustom.setAttribute("onChange", "changeStyle(this);");
-
         // -- adding fields to form & page --
         unstyled.add(twostate);
         unstyled.add(tristate);
         unstyled.add(custom);
+        unstyled.add(new Label("<i>Notations backed by resource bundle.</i>"));
         unstyled.add(notationTF);
         unstyled.add(notationYN);
         unstyled.add(notationOO);
@@ -74,15 +56,8 @@ public class BooleanSelectPage extends BorderPage {
         locForm.add(locale);
         locForm.add(new Label("<small><i>this will reset the form</i></small>"));
         locForm.add(new Submit("update", "update", this, "onUpdateClick"));
-        hp.add(unstyled);
 
-        styled.add(styledTwostate);
-        styled.add(styledTristate);
-        styled.add(new Label("<i>Images only work with Firefox.</i>"));
-        styled.add(styledCustom);
-        hp.add(styled);
-        
-        form.add(hp);
+        form.add(unstyled);
         form.add(sbOk);
         form.add(new PageSubmit("cancel", this.getClass()));
     }
@@ -91,13 +66,11 @@ public class BooleanSelectPage extends BorderPage {
         String l = getContext().getLocale().getLanguage();
         locale.setValue(l.toUpperCase());
         locale.setLabel("Change Locale (current: " + l + ")");
-        // hey! where is autoboxing gone? oh wait 1.4... ;-)
-        if (!sbOk.isClicked()) styledCustom.setBoolean(Boolean.FALSE);
     }
 
     public boolean onSubmitClick() {
         if (form.isValid()) {
-            result = "Tristate Select-values are: " + tristate.getBoolean().toString().toUpperCase() + " and " + styledTristate.getBoolean().toString().toUpperCase();
+            result = "Tristate value is: " + tristate.getBoolean().toString().toUpperCase();
         }
         return true;
     }
