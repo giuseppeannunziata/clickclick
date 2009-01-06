84f6f0bdb40204b2b1807da3eeef768c5d22a579
sabob1 <sabob1@gmail.com>
2009-01-06 14:46:10 +0000

    updated to new Apache namespace

--- clickclick/core/src/org/apache/click/ClickClickServlet.java
+++ clickclick/core/src/org/apache/click/ClickClickServlet.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.sf.click;
+package org.apache.click;
 
 import org.apache.click.AjaxControlRegistry;
 import org.apache.click.util.PageImports;
