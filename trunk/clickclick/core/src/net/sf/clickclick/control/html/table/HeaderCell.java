6d414ef96badfbc9de19f6cc7174306fcc90495a
sabob1@gmail.com <sabob1@gmail.com@ec524382-5d46-0410-b920-5d724188c503>
2009-05-01 21:25:41 +0000

    improved html table

--- trunk/clickclick/core/src/net/sf/clickclick/control/html/table/HeaderCell.java
+++ trunk/clickclick/core/src/net/sf/clickclick/control/html/table/HeaderCell.java
@@ -1,19 +1,45 @@
 package net.sf.clickclick.control.html.table;
 
+import org.apache.click.util.ClickUtils;
+import org.apache.click.util.HtmlStringBuffer;
+import org.apache.commons.lang.StringUtils;
+
 /**
  *
  * @author Bob Schellink
  */
 public class HeaderCell extends Cell {
 
+    private String headerTitle;
+
     public HeaderCell() {
     }
-    
+
     public HeaderCell(String name) {
         super(name);
     }
-    
-     public String getTag() {
+
+    public String getHeaderTitle() {
+        if (headerTitle == null) {
+            headerTitle = getMessage(getName() + ".headerTitle");
+        }
+        if (headerTitle == null) {
+            headerTitle = ClickUtils.toLabel(getName());
+        }
+        return headerTitle;
+    }
+
+    public String getTag() {
         return "th";
+    }
+
+    public void render(HtmlStringBuffer buffer) {
+        // If no child controls have been added and name is not blank, create a
+        // default child from the Cell name
+        if (!hasControls() && StringUtils.isNotBlank(getName())) {
+            setText(getHeaderTitle());
+        }
+
+        super.render(buffer);
     }
 }
