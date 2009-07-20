b908fbbec58e44c6bbd4440a3e9b3f244e3fce67
sabob1@gmail.com <sabob1@gmail.com@ec524382-5d46-0410-b920-5d724188c503>
2009-07-20 08:31:12 +0000

    moved Taconite to root folder

--- trunk/clickclick/jquery/src/net/sf/clickclick/jquery/Taconite.java
+++ trunk/clickclick/jquery/src/net/sf/clickclick/jquery/Taconite.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.sf.clickclick.jquery.util;
+package net.sf.clickclick.jquery;
 
 import java.io.StringReader;
 import java.util.ArrayList;
