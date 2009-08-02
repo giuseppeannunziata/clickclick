7ff50194d9d71ec56c5ae0c0bcc68390ded19556
sabob1@gmail.com <sabob1@gmail.com@ec524382-5d46-0410-b920-5d724188c503>
2009-08-02 12:44:53 +0000

    javadoc

--- trunk/clickclick/jquery-examples/src/net/sf/clickclick/examples/jquery/page/ajax/validation/JqueryValidation2Demo.java
+++ trunk/clickclick/jquery-examples/src/net/sf/clickclick/examples/jquery/page/ajax/validation/JqueryValidation2Demo.java
@@ -1,3 +1,16 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.sf.clickclick.examples.jquery.page.ajax.validation;
 
 import java.util.List;
@@ -31,8 +44,6 @@ import org.apache.click.extras.control.TelephoneField;
 /**
  * This example demonstrates how to validate a Form using using the jQuery
  * Validator plugin: http://docs.jquery.com/Plugins/Validation
- *
- * @author Bob Schellink
  */
 public class JqueryValidation2Demo extends BorderPage {
 
