45437018ca1338d244e93797673a46eeeb754eb6
sabob1 <sabob1@gmail.com>
2009-12-09 12:43:28 +0000

    modified demos to use new generic JavaScript templates

--- clickclick/jquery-examples/src/net/sf/clickclick/examples/jquery/page/controls/CalendarDemo.java
+++ clickclick/jquery-examples/src/net/sf/clickclick/examples/jquery/page/controls/CalendarDemo.java
@@ -13,14 +13,12 @@
  */
 package net.sf.clickclick.examples.jquery.page.controls;
 
-import java.util.List;
 import java.util.Map;
 import net.sf.clickclick.examples.jquery.control.ui.UICalendarField;
 import net.sf.clickclick.examples.jquery.page.BorderPage;
 import org.apache.click.control.Form;
 import org.apache.click.control.Submit;
 import org.apache.click.control.TextField;
-import org.apache.click.element.JsScript;
 import org.apache.click.util.ClickUtils;
 
 public class CalendarDemo extends BorderPage {
@@ -36,14 +34,8 @@ public class CalendarDemo extends BorderPage {
         form.add(new Submit("submit"));
     }
 
-    public List getHeadElements() {
-        if (headElements == null) {
-            headElements = super.getHeadElements();
-
-            // Add calendar-demo.js script
-            Map model = ClickUtils.createTemplateModel(this, getContext());
-            headElements.add(new JsScript("/controls/calendar-demo.js", model));
-        }
-        return headElements;
+    @Override
+    protected Map getJsTemplateModel() {
+       return ClickUtils.createTemplateModel(this, getContext());
     }
 }
