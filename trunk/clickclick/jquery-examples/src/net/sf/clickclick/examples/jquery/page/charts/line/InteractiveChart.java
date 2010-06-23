c13b3818e4a8bed8042857e4890099b74ed71005
sabob1@gmail.com <sabob1@gmail.com@ec524382-5d46-0410-b920-5d724188c503>
2010-06-23 14:09:12 +0000

    add serialization

--- trunk/clickclick/jquery-examples/src/net/sf/clickclick/examples/jquery/page/charts/line/InteractiveChart.java
+++ trunk/clickclick/jquery-examples/src/net/sf/clickclick/examples/jquery/page/charts/line/InteractiveChart.java
@@ -28,6 +28,8 @@ import org.apache.click.util.HtmlStringBuffer;
  */
 public class InteractiveChart extends BorderPage {
 
+    private static final long serialVersionUID = 1L;
+
     public List<Element> getHeadElements() {
         if (headElements == null) {
             headElements = super.getHeadElements();
