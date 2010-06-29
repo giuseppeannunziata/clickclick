56ef8a85b9cca22c8309077299f785893519c63f
sabob1@gmail.com <sabob1@gmail.com@ec524382-5d46-0410-b920-5d724188c503>
2010-06-29 15:37:21 +0000

    added convenience id attribute and fixed NPE

--- trunk/clickclick/core/src/net/sf/clickclick/control/html/Div.java
+++ trunk/clickclick/core/src/net/sf/clickclick/control/html/Div.java
@@ -70,17 +70,6 @@ public class Div extends AbstractContainer {
         }
     }
 
-    /**
-     * Create a div element with the given name and id.
-     *
-     * @param name the div name
-     * @param id the div id
-     */
-    public Div(String name, String id) {
-        this(name);
-        setAttribute("id", id);
-    }
-
     // ------------------------------------------------------ Public Properties
 
     /**
@@ -90,8 +79,20 @@ public class Div extends AbstractContainer {
      *
      * @return this controls html tag
      */
+    @Override
     public final String getTag() {
         return "div";
+    }
+
+    /**
+     * Set the HTML id attribute for the control with the given value.
+     *
+     * @param id the element HTML id attribute value to set
+     * @return this control instance
+     */
+    public Div id(String id) {
+        setId(id);
+        return this;
     }
 
     /**
