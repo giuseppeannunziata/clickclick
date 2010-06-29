aef778c2db708f6b41248dda1b36296b70ae4ef8
sabob1@gmail.com <sabob1@gmail.com@ec524382-5d46-0410-b920-5d724188c503>
2010-06-29 03:48:36 +0000

    updated dynamic image to use pageAction instead of actionListener. This is much faster to render images, since the onInit method is bypassed.

--- trunk/clickclick/core/src/net/sf/clickclick/control/DynamicImage.java
+++ trunk/clickclick/core/src/net/sf/clickclick/control/DynamicImage.java
@@ -13,17 +13,9 @@
  */
 package net.sf.clickclick.control;
 
-import java.awt.image.BufferedImage;
-import java.io.BufferedOutputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.Enumeration;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
-import javax.imageio.ImageIO;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.Map.Entry;
 import org.apache.click.Context;
 import org.apache.click.Page;
 import org.apache.click.util.ClickUtils;
@@ -31,19 +23,21 @@ import org.apache.click.util.HtmlStringBuffer;
 import org.apache.commons.lang.StringUtils;
 
 /**
- * Provides an abstract dynamic Image control: &nbsp; &lt;img src='chart?year=2009&width=200&height=200'&gt;.
+ * Provides a dynamic Image control: &nbsp; &lt;img src='chart.htm?pageAction=renderChart&year=2009&width=200&height=200'&gt;.
  * <p/>
  * The DynamicImage control is useful for generating and displaying dynamic
- * images. When specifying a DynamicImage you have to provide the method
- * {@link #getImageData()} and return the image data as a byte array. Below is
- * an example.
+ * images. When specifying a DynamicImage you have to specify a page action
+ * method that will be called from the browser. The page action method must
+ * return a Partial containing the image data. Below is an example.
  * <p/>
  *
  * <tt>DynamicImagePage.java</tt> page class:
  * <pre class="prettyprint">
  * public DynamicImagePage() {
  *
- *     DynamicImage image = new DynamicImage("dynamicImage") {
+ *     // renderChart is the name of the page method to invoke that will stream
+ *     // back the image data
+ *     DynamicImage image = new DynamicImage("dynamicImage", "renderChart") {
  *         public byte[] getImageData() {
  *             InputStream is = null;
  *
@@ -62,15 +56,31 @@ import org.apache.commons.lang.StringUtils;
  *        addControl(image);
  *     }
  *
- *     private byte[] generateChart() throws IOException {
- *         Context context = getContext();
+ *     // The Page Action method that returns a Partial containing the image data
+ *     public Partial renderChart() {
+ *         Partial partial = new Partial();
  *
- *         // This method returns a byte array that can be dynamically
- *         // retrieved from a database or generated on the fly.
- *         InputStream is = ...
+ *         // Set content type to a "png" image
+ *         partial.setContentType(ClickUtils.getMimeType(".png"));
  *
- *         return IOUtils.toByteArray(is);
+ *         // Set the partial bytes to image data
+ *         byte[] imageData = generateChart();
+ *         partial.setBytes(imageData);
+ *         return partial;
  *     }
+ *
+ *     private byte[] generateChart() {
+ *        // This method returns a byte array that can be dynamically retrieved
+ *        // from a database or generated on the fly.
+ *
+ *        try {
+ *            InputStream is = getContext().getServletContext().getResourceAsStream("/assets/images/chart.png");
+ *            return IOUtils.toByteArray(is);
+ *
+ *        } catch (IOException ioe) {
+ *            throw new RuntimeException(ioe);
+ *        }
+ *    }
  * } </pre>
  *
  * Below is the <tt>dynamic-image.htm</tt> template:
@@ -84,7 +94,7 @@ import org.apache.commons.lang.StringUtils;
  *
  * @see DynamicImage
  */
-public abstract class DynamicImage extends Image {
+public class DynamicImage extends Image {
 
     // -------------------------------------------------------------- Constants
 
@@ -105,28 +115,25 @@ public abstract class DynamicImage extends Image {
     /** The BMP constant "<tt>bmp</tt>". */
     public static final String BMP = "bmp";
 
-    /** The DynamicImage request indicator "<tt>requestImage</tt>". */
-    public static final String IMAGE_PARAM = "requestImage";
+    /** The DynamicImage request indicator "<tt>imageAction</tt>". */
+    public static final String IMAGE_ACTION_PARAM = "imageAction";
 
     // -------------------------------------------------------------- Variables
 
     /** The image request parameters. */
-    protected Map parameters;
+    protected Map<String, Object> parameters;
 
     /** The image type: {@link #PNG}, {@link #GIF} etc. */
     protected String imageType;
 
-    /**
-     * Indicates if the image should be rendered or not. This depends on
-     * whether the {@link #IMAGE_PARAM} request attribute matches the image name.
-     */
-    protected boolean renderImage;
+    /** The page action method that will serve the image data from. */
+    protected String pageAction;
 
     /**
-     * The target Page or URL from where the image data will be served from. If
+     * The target Page class that will serve the image data from. If
      * no value is specified, the target defaults to the current Page.
      */
-    protected String target;
+    protected Class pageClass;
 
     // ----------------------------------------------------------- Constructors
 
@@ -134,7 +141,7 @@ public abstract class DynamicImage extends Image {
      * Create a DynamicImage. The {@link #imageType} will default to {@link #PNG}.
      */
     public DynamicImage() {
-        this(null);
+        this(null, null);
     }
 
     /**
@@ -143,19 +150,8 @@ public abstract class DynamicImage extends Image {
      *
      * @param name the name of the control
      */
-    public DynamicImage(String name) {
-        this(name, null, "png");
-    }
-
-    /**
-     * Create a DynamicImage with the given name and ID attribute. The
-     * {@link #imageType} will default to {@link #PNG}.
-     *
-     * @param name the name of the image
-     * @param id the id attribute of the image
-     */
-    public DynamicImage(String name, String id) {
-        this(name, id, "png");
+    public DynamicImage(String name, String pageAction) {
+        this(name, pageAction, "png");
     }
 
     /**
@@ -165,14 +161,11 @@ public abstract class DynamicImage extends Image {
      * @param id the id attribute of the image
      * @param imageType the image type of the image
      */
-    public DynamicImage(String name, String id, String imageType) {
+    public DynamicImage(String name, String pageAction, String imageType) {
         if (name != null) {
             setName(name);
         }
-        if (id != null) {
-            setId(id);
-        }
-        setListener(this, "onRenderImage");
+        setPageAction(pageAction);
         setImageType(imageType);
     }
 
@@ -197,28 +190,13 @@ public abstract class DynamicImage extends Image {
     }
 
     /**
-     * Return the image request parameter value for the given name, or null if
-     * the parameter value does not exist.
-     *
-     * @param name the name of request parameter
-     * @return the link request parameter value
-     */
-    public String getParameter(String name) {
-        if (hasParameters()) {
-            return (String) getParameters().get(name);
-        } else {
-            return null;
-        }
-    }
-
-    /**
      * Set the image parameter with the given parameter name and value.
      *
      * @param name the attribute name
      * @param value the attribute value
      * @throws IllegalArgumentException if name parameter is null
      */
-    public void setParameter(String name, String value) {
+    public void setParameter(String name, Object value) {
         if (name == null) {
             throw new IllegalArgumentException("Null name parameter");
         }
@@ -230,62 +208,23 @@ public abstract class DynamicImage extends Image {
     }
 
     /**
-     * Return the DynamicImage parameters Map.
-     *
-     * @return the DynamicImage parameters Map
-     */
-    public Map getParameters() {
-        if (parameters == null) {
-            parameters = new HashMap(1);
-        }
-        return parameters;
-    }
-
-    /**
-     * Return true if the DynamicImage has parameters, false otherwise.
-     *
-     * @return true if the DynamicImage has parameters, false otherwise
-     */
-    public boolean hasParameters() {
-        return parameters != null && !parameters.isEmpty();
-    }
-
-    /**
      * Return the target uri where the image data will be served from.
      * <p/>
      * If no value is specified, the target defaults to the current Page.
      *
      * @return the target uri where the image data will be served from
      */
-    public String getTarget() {
-        return target;
+    public String getPageAction() {
+        return pageAction;
     }
 
     /**
-     * Set the target uri where the image data will be served from.
-     * <p/>
-     * If no value is specified, the target defaults to the current Page.
-     * <p/>
-     * If the target begins with a <tt class="wr">"/"</tt>
-     * character the target will be prefixed with the web applications
-     * <tt>context path</tt>. Note if the given target is already prefixed
-     * with the <tt>context path</tt>, Click won't add it a second time.
+     * Set the page action that will will serve the image data from.
      *
      * @param target the target uri where the image data will be served from
      */
-    public void setTarget(String target) {
-        Context context = getContext();
-        if (StringUtils.isNotBlank(target)) {
-            if (target.charAt(0) == '/') {
-                String contextPath = context.getRequest().getContextPath();
-
-                // Guard against adding duplicate context path
-                if (!target.startsWith(contextPath + '/')) {
-                    target = contextPath + target;
-                }
-            }
-        }
-        this.target = target;
+    public void setPageAction(String pageAction) {
+        this.pageAction = pageAction;
     }
 
     /**
@@ -296,15 +235,19 @@ public abstract class DynamicImage extends Image {
      * @param pageClass the target Page class where the image data will be served
      * from
      */
-    public void setTarget(Class pageClass) {
-        String target = getContext().getPagePath(pageClass);
+    public void setPageClass(Class pageClass) {
+        this.pageClass = pageClass;
+    }
 
-        // If page class maps to a jsp, convert to htm which allows ClickServlet
-        // to process the target
-        if (target != null && target.endsWith(".jsp")) {
-            target = StringUtils.replaceOnce(target, ".jsp", ".htm");
-        }
-        this.target = target;
+    /**
+     * Return the target Page class where the image data will be served from.
+     * <p/>
+     * If no value is specified, the target defaults to the current Page.
+     *
+     * @return the target Page class where the image data will be served from
+     */
+    public Class<? extends Page> getPageClass() {
+        return this.pageClass;
     }
 
     /**
@@ -315,30 +258,43 @@ public abstract class DynamicImage extends Image {
      *
      * @return the src attribute
      */
+    @Override
     public String getSrc() {
-        String uri = getTarget();
-        Context context = getContext();
-        if (StringUtils.isBlank(target)) {
-            uri = ClickUtils.getRequestURI(context.getRequest());
+        String pageAction = getPageAction();
+        if (StringUtils.isBlank(pageAction)) {
+            throw new IllegalStateException("PageAction must be defined");
         }
 
-        HtmlStringBuffer buffer = new HtmlStringBuffer(uri.length() + getName().length() + 40);
-        buffer.append(uri);
-        buffer.append("?");
-        buffer.append(IMAGE_PARAM);
-        buffer.append("=");
-        buffer.append(getName());
-        if (hasParameters()) {
-            String encodedValue;
-            for (Iterator i = getParameters().keySet().iterator(); i.hasNext(); buffer.append(encodedValue)) {
-                buffer.append("&amp;");
-                String name = i.next().toString();
-                Object paramValue = getParameters().get(name);
-                encodedValue = ClickUtils.encodeUrl(paramValue, context);
-                buffer.append(name);
-                buffer.append("=");
-            }
+        String pagePath = null;
 
+        Context context = getContext();
+        Class pageClass = getPageClass();
+        if (pageClass == null) {
+            pagePath = ClickUtils.getRequestURI(context.getRequest());
+        } else {
+            pagePath = context.getPagePath(pageClass);
+        }
+
+        // If page class maps to a jsp, convert to htm which allows ClickServlet
+        // to process the target
+        if (pagePath != null && pagePath.endsWith(".jsp")) {
+            pagePath = pagePath.replace(".jsp", ".htm");
+        }
+
+        HtmlStringBuffer buffer = new HtmlStringBuffer(pagePath.length() + getName().length() + 40);
+        buffer.append(pagePath);
+        buffer.append("?");
+        buffer.append(Page.PAGE_ACTION);
+        buffer.append("=");
+        buffer.append(pageAction);
+        if (hasParameters()) {
+
+            for (Entry<String, Object> entry : getParameters().entrySet()) {
+                buffer.append("&amp;");
+                buffer.append(entry.getKey());
+                buffer.append("=");
+                buffer.append(ClickUtils.encodeUrl(entry.getValue(), context));
+            }
         }
         return context.getResponse().encodeURL(buffer.toString());
     }
@@ -351,112 +307,33 @@ public abstract class DynamicImage extends Image {
      * @param src the src attribute of the image
      * @throws UnsupportedOperationException if invoked
      */
+    @Override
     public void setSrc(String src) {
         throw new UnsupportedOperationException("DynamicImage does not support"
             + " the 'src' attribute. For static images rather use the Image"
             + " control.");
     }
 
-    // --------------------------------------------------------- Public Methods
+    // Private Methods --------------------------------------------------------
 
     /**
-     * Abstract method that must return the Image content as a byte array.
-     * For example:
+     * Return the DynamicImage parameters Map.
      *
-     * <pre class="prettyprint">
-     * InputStream is = getContext().getServletContext().getResourceAsStream("/assets/images/chart.png");
-     * return IOUtils.toByteArray(is); </pre>
-     *
-     * @return the image content as a byte array
+     * @return the DynamicImage parameters Map
      */
-    public abstract byte[] getImageData();
-
-    /**
-     * Utility method to convert an AWT {@link java.awt.image.BufferedImage}
-     * to a byte array.
-     *
-     * @param bufferedImage the buffered image instance
-     * @return the bufferedImage as a byte array
-     */
-    public byte[] toImageData(BufferedImage bufferedImage) {
-        try {
-            ByteArrayOutputStream outputStream;
-            outputStream = new ByteArrayOutputStream();
-            ImageIO.write(bufferedImage, getImageType(), outputStream);
-            return outputStream.toByteArray();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    private Map<String, Object> getParameters() {
+        if (parameters == null) {
+            parameters = new HashMap(1);
         }
+        return parameters;
     }
 
     /**
-     * The image {@link #setListener(java.lang.Object, java.lang.String) listener}
-     * implementation.
-     * <p/>
-     * This method delegates to {@link #getImageData()} to return the image data
-     * as a byte array.
+     * Return true if the DynamicImage has parameters, false otherwise.
      *
-     * @return true to continue Page event processing or false otherwise
+     * @return true if the DynamicImage has parameters, false otherwise
      */
-    public boolean onRenderImage() {
-        byte imageData[];
-        boolean flag;
-        Page page;
-        imageData = getImageData();
-        try {
-            HttpServletResponse response = getContext().getResponse();
-            response.setContentType("image/" + getImageType());
-            java.io.OutputStream outputStream = response.getOutputStream();
-            BufferedOutputStream bout = new BufferedOutputStream(outputStream);
-            bout.write(imageData);
-            bout.flush();
-            bout.close();
-            flag = true;
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
-        } finally {
-            page = ClickUtils.getParentPage(this);
-            page.setPath(null);
-        }
-        return flag;
-    }
-
-    /**
-     * Bind the DynamicImage request values. This method will only bind the
-     * image parameters if the {@link #IMAGE_PARAM} request attribute value
-     * matches the image name.
-     */
-    public void bindRequestValue() {
-        if (getContext().isMultipartRequest()) {
-            return;
-        }
-        renderImage = getName().equals(getContext().getRequestParameter(IMAGE_PARAM));
-        if (renderImage) {
-            HttpServletRequest request = getContext().getRequest();
-            String name;
-            String value;
-            for (Enumeration paramNames = request.getParameterNames(); paramNames.hasMoreElements(); getParameters().put(name, value)) {
-                name = paramNames.nextElement().toString();
-                value = request.getParameter(name);
-            }
-        }
-    }
-
-    /**
-     * @see org.apache.click.Control#onProcess()
-     *
-     * @return true to continue Page event processing or false otherwise
-     */
-    public boolean onProcess() {
-        bindRequestValue();
-        if (renderImage) {
-            if (listener != null && listenerMethod != null) {
-                return ClickUtils.invokeListener(listener, listenerMethod);
-            } else {
-                return true;
-            }
-        } else {
-            return true;
-        }
+    private boolean hasParameters() {
+        return parameters != null && !parameters.isEmpty();
     }
 }
